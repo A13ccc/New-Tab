00efa5e9712da15ee913efb85e33d7ccf895d43f
Gl1tch981 <52516208+Gl1tch981@users.noreply.github.com>
2024-02-09 22:55:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
 document.getElementById('search-button').addEventListener('click', function() {
-            performSearch();
-        });
-
-        document.getElementById('search-input').addEventListener('keypress', function(event) {
-            if (event.key === 'Enter') {
-                performSearch();
-            }
-        });
-
-        function performSearch() {
-            const searchQuery = document.getElementById('search-input').value;
-            
-            // Replace 'YOUR_URL_PART' with the part of the URL you want to replace
-            const baseUrl = 'https://www.qwant.com/?q=<CSEARCHINPUT>&t=web';
-    
-            // Replace the part of the URL with the search query
-            const updatedUrl = baseUrl.replace('<CSEARCHINPUT>', encodeURIComponent(searchQuery));
-    
-            // Open the updated URL in a new tab
-            window.location.href = updatedUrl;
-        }
+    var query = document.getElementById('search-input').value.trim();
+    if (query !== "") {
+        var searchUrl = "https://www.qwant.com/?q=" + encodeURIComponent(query);
+        window.location.href = searchUrl;
+    }
+});
