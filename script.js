79e1e7b1c0714ce64f193cbbaf4091d43a5193df
A13c <pobloplays@gmail.com>
2024-02-28 22:49:57 -0500

    no

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ document.getElementById('search-input').addEventListener('keypress', function(ev
   }
 });
 
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  window.location.href = "mobile.html";
+}
+
 const addBookmarkButton = document.getElementById('add-bookmark');
 addBookmarkButton.addEventListener('click', handleBookmarkClick);
 
